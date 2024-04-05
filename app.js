39d6b0bbef312783ad0c7fa1dedf11826655cca0
oriolorriols <oriol.orriols@gmail.com>
2024-04-05 19:21:17 +0200

    Cors

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors(corsOptions));
+
+var corsOptions = { 
+    origin: '*',
+}
+
+
+
+app.get('/tasks')
 
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
