cbf6fcde69c436a1e45aed1a1e7b128b6a3ec83d
Oriol Orriols <oriol.orriols@gmail.com>
2026-02-07 10:42:34 +0100

    Update .env and Bot

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,8 @@
-const taskModel = require('../models/task.model')
+require('dotenv').config()
 
+const taskModel = require('../models/task.model')
 const schedule = require('node-schedule')
+
 
 const getTasks = async (req, res) => {
     const allTasks = await taskModel.find()
@@ -10,45 +12,48 @@ const getTasks = async (req, res) => {
 
 const getTasksbyID = (req, res) => {
     taskModel.findOne(req.params)
-   .then(task => res.status(202).json(task))
-   .catch(err => res.status(500).json(err))
+        .then(task => res.status(202).json(task))
+        .catch(err => res.status(500).json(err))
 }
 
 const addTask = (req, res) => {
     taskModel.create(req.body)
-    .then(task => res.status(202).json(task))
-    .catch(err => res.status(500).json(err.errorResponse.errmsg))
+        .then(task => res.status(202).json(task))
+        .catch(err => res.status(500).json(err.errorResponse.errmsg))
 }
 
 const updateTask = (req, res) => {
     const data = req.body
-    taskModel.findOneAndUpdate(req.params, data, {new: true})
-    .then(task => res.status(202).json(task))
-    .catch(err => res.status(500).json(err))
+    taskModel.findOneAndUpdate(req.params, data, { new: true })
+        .then(task => res.status(202).json(task))
+        .catch(err => res.status(500).json(err))
 }
 
 const deleteTask = (req, res) => {
     taskModel.findOneAndDelete(req.params)
-    .then(task => {
-        if(task){
-            res.status(202).json(`Deleted task: ${task.id}`)
-        } else {
-            res.status(404).json('This task does not exist')}
+        .then(task => {
+            if (task) {
+                res.status(202).json(`Deleted task: ${task.id}`)
+            } else {
+                res.status(404).json('This task does not exist')
+            }
         })
-    .catch(err => res.status(500).json(err))
+        .catch(err => res.status(500).json(err))
 }
 
 async function backendBot(req, res) {
-    try {
-        const response = await fetch('https://todo-app-back-20dl.onrender.com/tasks')
-        const data = await response.json();
-        console.log('El bot está fucionando cada 15 min');
-    } catch (error) {
-        console.log('Error: ', error);
+    if (!process.env.IS_LOCAL) {
+        try {
+            const response = await fetch('https://todo-app-back-20dl.onrender.com/tasks')
+            const data = await response.json();
+            console.log('Bot is running every 15 minutes');
+        } catch (error) {
+            console.log('Error: ', error);
+        }
     }
 }
 
-schedule.scheduleJob('*/15 * * * *', async () => { 
+schedule.scheduleJob('*/15 * * * *', async () => {
     await backendBot();
 });
 
